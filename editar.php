beb9294287d742476807bb728c4eda91033d4218
jjavierChp <86015424+jjavierChp@users.noreply.github.com>
2021-10-18 15:19:15 +0200

    Add files via upload
    path: /libros/view
    
    Lo subo, aunque no recuerdo si llegué a cambiar algo

--- editar.php
+++ editar.php
@@ -1,65 +1,96 @@
-<?php
-require_once(__DIR__ . '/../conexion/conexion.php');
-require_once(__DIR__ . '/../view/head.php');
-require_once(__DIR__ . '/model/db.php');
-require_once(__DIR__ . '/../autores/model/db.php');
-require_once(__DIR__ . '/model/libro.php');
-
-$idLibro = $_GET["idLibro"];
-
-$libro = librosBuscarUno($idLibro);
-$autores = autoresBuscarTodos();
-
-// En caso de que la solicitud sea un post (es decir, se haya enviado el form con los datos para el nuevo préstamo)
-// Ejecutamos el código para agregar el nuevo préstamo en la base de datos.
-if (isset($_POST) && count($_POST) > 0) {
-
-    if (isset($_POST['titulo'])) {
-        $titulo = $_POST['titulo'];
-    } else {
-        // error
-    }
-    if (isset($_POST['editorial'])) {
-        $editorial = $_POST['editorial'];
-    } else {
-        // error
-    }
-    if (isset($_POST['genero'])) {
-        $genero = $_POST['genero'];
-    } else {
-        // error
-    }
-    if (isset($_POST['isbn'])) {
-        $isbn = $_POST['isbn'];
-    } else {
-        // error
-    }
-    if (isset($_POST['anhoPublicacion'])) {
-        $anhoPublicacion = $_POST['anhoPublicacion'];
-    } else {
-        // error
-    }
-    if (isset($_POST['fechaAlta'])) {
-        $fechaAlta = $_POST['fechaAlta'];
-    } else {
-        // error
-    }
-    if (isset($_POST['autores'])) {
-        $autores = $_POST['autores'];
-    } else {
-        // error
-    }
-    
-
-    $resultado = librosActualizar($titulo, $editorial, $genero, $isbn, $anhoPublicacion, $fechaAlta, $autores, $idLibro);
-
-    if ($resultado) {
-        agregarMensaje("Libro actualizado correctamente", "success");
-    }
-}
-if (hayMensajes()) {
-    mostrarMensajes();
-}
-
-require(__DIR__ . '/view/editar.php');
-require_once(__DIR__ . '/../view/footer.php');
+<div class="row">
+    <div class="col-12 col-sm-10 col-xl-6 mx-md-auto my-4">
+        <form method="post" action="/biblioteca/libros/editar.php">
+            <div class="card rounded-top border-dark bg-transparent">
+                <div class="card-title bg-naranja p-3 rounded-top mb-0">
+                    <h1 class="mb-0"><i class="bi bi-book me-2 fs-1"></i>Editar libro</h1>
+                </div>
+                <div class="card-body bg-white rounded-bottom">
+                    <div class="row mb-3">
+                        <div class="col-12 col-lg-7">
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <label for="titulo" class="form-label">
+                                        <i class="bi bi-bookmark-check me-2"></i>Título
+                                    </label>
+                                    <input name="titulo" type="text" value="<?= $libro['titulo'] ?>"
+                                        class="form-control">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <label for="editorial" class="form-label">
+                                        <i class="bi bi-bookmark-heart me-2"></i>Editorial
+                                    </label>
+                                    <input name="editorial" type="text" value="<?= $libro['editorial'] ?>" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <!--genero-->
+                                <div class="col">
+                                    <label for="genero" class="form-label">
+                                        <i class="bi bi-emoji-smile me-2"></i>Género
+                                    </label>
+                                    <select class="form-select" aria-label="Género" name="genero">
+                                        <option value="<?= $libro['genero'] ?>" selected>Género</option>
+                                        <?php
+                                        foreach (GENEROS_LITERARIOS as $indice => $nombre) {
+                                        ?>
+                                        <option value="<?= $indice ?>"><?= $nombre ?></option>
+                                        <?php
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <!--año edicion-->
+                                <div class="col">
+                                    <label for="anhoPublicacion" class="form-label">
+                                        <i class="bi bi-clock me-2"></i>Año de edición
+                                    </label>
+                                    <input name="anhoPublicacion" min="1000" max="2999" type="number"
+                                        value="<?= $libro['anhoPublicacion'] ?>" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <!--isbn-->
+                                    <label for="isbn" class="form-label">
+                                        <i class="bi bi-file-earmark-word me-2"></i>ISBN
+                                    </label>
+                                    <input name="isbn" type="text" value="<?= $libro['isbn'] ?>" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-5">
+                            <div class="row">
+                                <div class="col">
+                                    <label for="autores" class="form-label">
+                                        <i class="bi bi-file-person me-2"></i>Autores
+                                    </label>
+                                    <select class="form-select" multiple placeholder="Autores" size="14"
+                                        aria-label="Autores" name="autores[]">
+                                        <?php
+                                        foreach ($autores as $autor) {
+                                            $nombreAutor = $autor['nombre'];
+                                            if (isset($autor['apellidos'])) {
+                                                $nombreAutor .= " " . $autor['apellidos'];
+                                            }
+                                            echo '<option value="' . $autor['idAutor'] . '"selected>' . $nombreAutor . '</option>';
+                                        }
+                                        ?>
+                                    </select>
+                                    <span class="text-muted" style="font-size: .75rem;">* Mantén pulsada la tecla Ctrl
+                                        para
+                                        seleccionar a más
+                                        de un
+                                        autor</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn mt-3 bg-naranja"><i class="bi bi-save me-2"></i>Guardar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
